64fababddc5b37591c17bec996fff4a87dc723f3
Jung Nam <j.nam0808@gmail.com>
2021-03-19 12:31:51 -0400

    got the prompts working and uploading it on github

--- script.js
+++ script.js
@@ -3,21 +3,41 @@
 //const specialCharacter =
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  const test = confirm("do you see me?");
-  console.log("test: ", test);
-  password = "aa";
-  passwordText.value = password;
-}
-
 function generatePassword() {
-  return "Test Passwprd";
+  var sizeOfPasswordQuestion = prompt(
+    "Please enter the length of the password between 8 and 128 characters"
+  );
+  if (sizeOfPasswordQuestion < 8) {
+    alert("Please enter number greater than 8 characters!");
+  } else if (sizeOfPasswordQuestion > 128) {
+    alert("Please enter number less than 128 characters!");
+  }
+  const lowerCaseQuestion = confirm("Do you want lowercase in your password?");
+  const upperCaseQuestion = confirm("Do you want uppercase in your password?");
+  const numericQuestion = confirm("Do you want nummbers in your password?");
+  const specialCharacterQuestion = confirm(
+    "Do you want special character in your password?"
+  );
 }
+// console.log(sizeOfPassword);
+// if (sizeOfPassword < 8) {
+//   alert("Please enter number greater than 8 characters!");
+// }
+// const test = prompt("do you see me?");
+// console.log("test: ", test);
+// password = "aa";
+// passwordText.value = password;
+
+// function generatePassword() {
+//   return "Test Passwprd";
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
+// console.log(Math.floor(Math.random() * 26));
+
+//console.log(Math.random());
