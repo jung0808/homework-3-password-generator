9607db2c41e7b4bcbbcd7879c9ed64e2f194f186
Jung Nam <j.nam0808@gmail.com>
2021-03-19 21:07:52 -0400

    made more progress. got the prompts working and a bit more

--- script.js
+++ script.js
@@ -1,6 +1,73 @@
-// const upperCase =
-//const lowercase =
-//const specialCharacter =
+const upperCaseArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const lowerCaseArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const numericArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacterArr = [
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+];
 
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
@@ -12,16 +79,53 @@ function generatePassword() {
   );
   if (sizeOfPasswordQuestion < 8) {
     alert("Please enter number greater than 8 characters!");
+    return; //generatepassword();
   } else if (sizeOfPasswordQuestion > 128) {
     alert("Please enter number less than 128 characters!");
+    return;
   }
+
   const lowerCaseQuestion = confirm("Do you want lowercase in your password?");
   const upperCaseQuestion = confirm("Do you want uppercase in your password?");
   const numericQuestion = confirm("Do you want nummbers in your password?");
   const specialCharacterQuestion = confirm(
     "Do you want special character in your password?"
   );
+
+  let possibleCharacters = [];
+
+  if (lowerCaseQuestion) {
+    possibleCharacters = possibleCharacters.concat(lowerCaseArr);
+    //add lowercase to possibleCharacters. if False do not add.
+    console.log(possibleCharacters);
+    console.log(lowerCaseArr.length);
+    // var index = Math.floor(Math.random() * lowerCaseArr(possiblecharacters).length);
+  }
+  if (upperCaseQuestion) {
+    possibleCharacters = possibleCharacters.concat(upperCaseArr);
+    console.log(possibleCharacters);
+    console.log(upperCaseArr.length);
+  }
+  if (numericQuestion) {
+    possibleCharacters = possibleCharacters.concat(numericArr);
+    console.log(possibleCharacters);
+    console.log(numericArr.length);
+  }
+  if (specialCharacterQuestion) {
+    possibleCharacters = possibleCharacters.concat(specialCharacterArr);
+    console.log(possibleCharacters);
+    console.log(specialCharacterArr.length);
+  }
 }
+
+// let randomPassword = ""
+//  //loop over possible characters;
+//  for (let i = 0; i < sizeOfPasswordQuestion)
+
+//  return randomPassword;
+// }
+
+// js string to numbers
 // console.log(sizeOfPassword);
 // if (sizeOfPassword < 8) {
 //   alert("Please enter number greater than 8 characters!");
